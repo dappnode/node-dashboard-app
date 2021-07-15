d75d11cd7b87edf5e0d5b5a8930bd7db5dbed266
Amin Latifi <a.latifi.al@gmail.com>
2021-07-15 20:39:08 +0430

    Change Network Label place in stake card

--- src/components/PoolCard.tsx
+++ src/components/PoolCard.tsx
@@ -114,8 +114,8 @@ const Principal = ({
 		<>
 			<div style={{ width: '96%' }}>
 				<SpaceBetween>
-					<label>{platform}</label>
 					<NetworkLabel network={network} />
+					<label>{platform}</label>
 				</SpaceBetween>
 				<h1>
 					<img alt='logo' src={logo} /> {name}
