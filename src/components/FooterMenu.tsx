c5ef06b3b901c9c550242ef88b6752350d523d53
Marcelino García <mgarciate@gmail.com>
2021-07-16 09:25:04 +0200

    Fixed #39 Footer - Links to main page, discord, github and forum

---
+++ src/components/FooterMenu.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import styled from 'styled-components'
+import FooterMenuItem from './FooterMenuItem'
+
+const FooterMenu = () => (
+	<FooterMenuSection>
+		<Container>
+			<div />
+			<div>
+				<FooterMenuItem
+					title='Forum'
+					icon='/assets/external-link.svg'
+					link='https://discourse.dappnode.io'
+				/>
+				<FooterMenuItem
+					title='Github'
+					icon='/assets/github-link.svg'
+					link='https://github.com/dappnode'
+				/>
+				<FooterMenuItem
+					title='Discord'
+					icon='/assets/discord-link.svg'
+					link='https://discord.gg/c28an8dA5k'
+				/>
+				<FooterMenuItem
+					title='Twitter'
+					icon='/assets/twitter-link.svg'
+					link='https://twitter.com/dappnode'
+				/>
+			</div>
+		</Container>
+	</FooterMenuSection>
+)
+
+const FooterMenuSection = styled.section`
+	background-color: transparent;
+	text-align: center;
+	overflow: hidden;
+	margin: 20px 0px;
+	@media only screen and (max-width: 768px) {
+		height: auto;
+	}
+`
+
+const Container = styled.div`
+	width: 95%;
+	margin: auto;
+	height: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	flex-direction: row;
+`
+
+export default FooterMenu
---
+++ src/components/FooterMenuItem.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import styled from 'styled-components'
+
+type FooterMenuItemProps = {
+	title: string
+	icon: string
+	link: string
+}
+function FooterMenuItem({ title, icon, link }: FooterMenuItemProps) {
+	return (
+		<Item href={link} target='_blank'>
+			{title}
+			<img alt='link' src={icon} />
+		</Item>
+	)
+}
+
+const Item = styled.a`
+	font-family: Inter;
+	font-style: normal;
+	font-weight: normal;
+	font-size: 14px;
+	align-items: center;
+	text-align: center;
+	letter-spacing: 0.2px;
+	color: #222a29;
+	padding: 13px;
+	img {
+		margin-left: 4px;
+	}
+`
+
+export default FooterMenuItem
