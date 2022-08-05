375d8e98e10c1f7ca0bfe37701ff73937cabb1c0
Alex Peterson <alexpeterson91@gmail.com>
2022-08-05 04:20:21 -0500

    update to gnosis chain from xDai

--- src/lib/networkProvider.ts
+++ src/lib/networkProvider.ts
@@ -1,10 +1,10 @@
 import { JsonRpcProvider } from '@ethersproject/providers'
 import config from '../configuration'
 
-const { MAINNET_CONFIG, NETWORKS_CONFIG, XDAI_CONFIG } = config
+const { MAINNET_CONFIG, NETWORKS_CONFIG, GNOSIS_CONFIG } = config
 
 export const mainnetProvider = new JsonRpcProvider(MAINNET_CONFIG.nodeUrl)
-export const xdaiProvider = new JsonRpcProvider(XDAI_CONFIG.nodeUrl)
+export const gnosisProvider = new JsonRpcProvider(GNOSIS_CONFIG.nodeUrl)
 
 // eslint-disable-next-line no-underscore-dangle
 const _networksProviders = {}
